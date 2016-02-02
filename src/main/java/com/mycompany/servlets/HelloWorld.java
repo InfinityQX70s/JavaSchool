9fe559e29ade37983f6cf3150860b0d1c8afc3d0
InfinityQX70s <mazumisha@gmail.com>
2016-02-02 23:12:13 +0300

    Init commit

---
+++ src/main/java/com/mycompany/servlets/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.mycompany.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by infinity on 02.02.16.
+ */
+public class HelloWorld extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello World from Servlet!</h1>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: infinity
+  Date: 02.02.16
+  Time: 22:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+ <h1>Hello World!</h1>
+</body>
+</html>
